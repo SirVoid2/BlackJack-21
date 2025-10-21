b5a6744e6765103959b423e54bd99db2b492d406
SirVoid2 <techandbiketips@gmail.com>
2025-10-21 11:10:52 -0500

    j

--- js/main.js
+++ js/main.js
@@ -1,24 +1,11 @@
 // Starting game board values
 var cardsInDeck;
 
-$(document).ready(function() {
+$( document ).ready(function() {
   getCards();
   cardsInDeck = cards;
   updateVisibleChipBalances();
-
-  // Initialize Materialize modal
-  M.Modal.init(document.querySelectorAll('.modal'));
-
-  // Setup event handlers
-  setupAuthState();
-  setupWagerHandlers();
-  setupGameHandlers();
-  setupRules();
-
-  // Additional initializations if needed
 });
-
-// Your existing game logic:
 
 var currentTurn = "player";
 var currentWager = 0;
@@ -30,27 +17,28 @@ var isGameOver = false;
 var dealerHand = [];
 var dealerHandTotal = 0;
 var dealerGameBoard = $("#dealer");
-var dealerStatus = "start"; // Possible statuses: start, stand, hit
+var dealerStatus = "start"; // Possible statuses are start (initial gameplay), stand, hit
 
 // Player hand and starting totals
 var playerHand = [];
 var playerHandTotal = 0;
 var playerGameBoard = $("#user-hand");
 var playerHandTotalDisplay = $(".hand-total");
-var playerStatus = "start"; // Possible statuses: start, stand, hit
+var playerStatus = "start";  // Possible statuses are start (initial gameplay), stand, hit
 
-// Because aces can be 1 or 11, need to know if player has aces
+// Because aces can equal 1 or 11, need to quickly know if player has aces so we can
+// adjust value from 11 to 1 if they go over 21 (default value is 11)
 var playerHasAce = false;  
 
-// Player split game variables
-var splitGame = false; // default false
+// Player split game variables only used if the player splits their hand
+var splitGame = false; // default value is false, must be turned true
 var playerSplitHand = [];
 var playerSplitHandTotal = 0;
 var playerSplitGameBoard = $("#user-split-hand");
 var playerSplitHandTotalDisplay = $(".split-hand-total");
 var playerSplitStatus;
 
-// Buttons from DOM
+// Buttons pulled from DOM
 var startButton = $("#start-game-button");
 var doubleDownButton = $("#double-down-button");
 var hitButton = $("#hit-button");
@@ -58,93 +46,99 @@ var standButton = $("#stand-button");
 var splitButton = $(".split-button");
 var playAgainButton = $(".new-game-button"); 
 
-// Deactivates a button (event listener + appearance)
+// Deactivates a button (both event listener and appearance)
 function disableButton(buttonName) {
-  $(buttonName).off();
-  $(buttonName).addClass("disabled-button");
+	$(buttonName).off();
+	$(buttonName).addClass("disabled-button");
 }
 
-// Activates a button (event listener + appearance)
+// Activates a button (both event listener and appearance)
 function enableButton(buttonName, event) {
-  $(buttonName).click(event);
-  $(buttonName).removeClass("disabled-button");
+	$(buttonName).click(event);
+	$(buttonName).removeClass("disabled-button");
 }
 
-// Update chip balances displayed
+// Update chip totals displayed to user throughout the game
 function updateVisibleChipBalances() {
-  $(".current-wager").text(currentWager);
-  $(".current-chip-balance").text(currentChipBalance);
-  localStorage.setItem('blackjackChips', currentChipBalance);
+	$(".current-wager").text(currentWager);
+	$(".current-chip-balance").text(currentChipBalance);
+	localStorage.setItem('blackjackChips', currentChipBalance);
 }
 
-// Update hand totals displayed
+// Update card hand totals displayed to user throughout the game
 function updateVisibleHandTotals() {
-  $(playerHandTotalDisplay).text(playerHandTotal);
-  $(playerSplitHandTotalDisplay).text(playerSplitHandTotal);
+	$(playerHandTotalDisplay).text(playerHandTotal);
+	$(playerSplitHandTotalDisplay).text(playerSplitHandTotal);
 
-  // Show only 1st dealer card value if game not over
-  if (dealerHand.length === 2 && isGameOver === false && dealerStatus === "start") {
-    $(".dealer-hand-total").text(dealerHandTotal - dealerHand[1].value);
-  } else {
-    $(".dealer-hand-total").text(dealerHandTotal);
-  }
+	// If the dealer has not played yet or game is not over, only show value of 1st card
+	// as the player is still making their initial moves
+	if (dealerHand.length === 2 && isGameOver === false && dealerStatus === "start") {
+		$(".dealer-hand-total").text(dealerHandTotal - dealerHand[1].value);
+	} else {
+		$(".dealer-hand-total").text(dealerHandTotal);
+	}
+
 }
 
-// Called when player clicks on a wager chip
+// Called when player clicks on a chip
 function selectWager(amount){
-  currentWager = amount;
-  updateVisibleChipBalances();
+	currentWager = amount;
+	updateVisibleChipBalances();
 }
 
-// Flip dealer's hidden card
+// 	ANIMATIONS/INTERACTIVITY:
 function flipHiddenCard() {
-  if (dealerHand.length === 2) {
-    $("#dealer-card-1").addClass("flipped");
-    setTimeout(function(){
-      $("#dealer-card-1").attr("src", "img/" + dealerHand[1].src);
-      updateVisibleHandTotals();
-    }, 250);	
-  } 
+	// If it's just the initial round, first we need to flip/reveal the hidden dealer card when this is called
+	if (dealerHand.length === 2) {
+		$("#dealer-card-1").addClass("flipped");
+		setTimeout(function(){
+			$("#dealer-card-1").attr("src", "img/" + dealerHand[1].src);
+			updateVisibleHandTotals();
+		}, 250);	
+	} 
 }
 
-// In split mode, shrink inactive deck
+// Used in split game mode, shrinks the inactive deck and totals
 function scaleDownDeck(deck, totalDisplay) {
-  $(totalDisplay).addClass("splithand-scaledown");
-  $(deck).addClass("splithand-scaledown");
+	$(totalDisplay).addClass("splithand-scaledown");
+	$(deck).addClass("splithand-scaledown");
 }
 
-// Enlarge deck in split mode
+// Used in split game mode, enlarges the deck and totals when turn active or when
+// dome with gameplay
 function enlargeDeck(deck, totalDisplay) {
-  $(totalDisplay).removeClass("splithand-scaledown");
-  $(deck).removeClass("splithand-scaledown");
+	$(totalDisplay).removeClass("splithand-scaledown");
+	$(deck).removeClass("splithand-scaledown");
 }
 
-// Toggle rules overlay
+// Toggling rules from main nav gives an animation effect
 $(".rules-nav").click(function(){
-  $("#rules").toggle("blind", 500);
+	$("#rules").toggle("blind", 500);
 });
 
+// But clicking close does not provide an animation effect
 $("#rules-close").click(function(){
-  $("#rules").hide();
+	$("#rules").hide();
 });
 
-// Initialize Materialize modal
+// Materialize modal
 $(".modal").modal({ 
-  dismissible: false, 
-  opacity: .40, 
-  inDuration: 300, 
-  outDuration: 200, 
-  startingTop: "10%", 
-  endingTop: "10%", 
-});
+      dismissible: false, 
+      opacity: .40, 
+      inDuration: 300, 
+      outDuration: 200, 
+      startingTop: "10%", // Starting top style attribute
+      endingTop: "10%", // Ending top style attribute
+    }
+  );
 
-// Event listeners for wager chips
+// EVENT LISTENERS:
 $("#chip-10").click(function(){selectWager(10)});
 $("#chip-25").click(function(){selectWager(25)});
 $("#chip-50").click(function(){selectWager(50)});
 $("#chip-100").click(function(){selectWager(100)});
 
-// Button handlers
+// Button activation
 $(startButton).click(startGame);
 $(doubleDownButton).click(doubleDown); 
 $(hitButton).click(hit);
@@ -152,17 +146,8 @@ $(standButton).click(stand);
 $(playAgainButton).click(newGame);
 $("#reset-game").click(resetGame);
 
-$(".reduce-aces-button").click(function(){
-  reduceAcesValue(playerHand);
-  disableButton(splitButton);
-});
-
-// Setup game handlers (assuming these functions are defined elsewhere)
-function setupGameHandlers() {
-  // Implement your game event handlers or call existing functions here if needed
-}
-
-// Setup rules overlay
-function setupRules() {
-  // Already handled above
-}
+$(".reduce-aces-button").click(   // Can only see this if player draws 2 aces, would only be reducing in 1st deck
+	function(){
+		reduceAcesValue(playerHand);
+		disableButton(splitButton, split);
+}); 
